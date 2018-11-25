6a8de959f043ce453ceafdb0ba00b232d14fc80e
github username <ianjpollack@gmail.com>
2018-11-24 23:50:53 -0500

    shell build, godcontainer working

---
+++ src/containers/GodContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class GodContainer extends React.Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+
+  render() {
+    return(
+      <div id='god-container'>
+        yay today!
+      </div>
+    )
+  }
+
+}
+
+export default GodContainer;
